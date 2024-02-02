28106b0735875d0242c3b230ab90a28df3a85c45
IslamElzneny <eslam.elzneny@creatifyteam.com>
2024-02-02 23:38:13 +0200

    add attr function in the user model

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -25,5 +25,16 @@ class UserSeeder extends Seeder
             'email_verified_at' => '2024-02-02 14:26:10',
             'remember_token' => 'token',
         ]);
+        User::create([
+            'first_name' => 'عمر محمد',
+            'last_name' => 'زياد',
+            'email' => 'staff@demo.com',
+            'phone' => '0124574556',
+            'hire_date' => '2024-02-02 14:26:10',
+            'role' => 2,
+            'password' => Hash::make('1234'),
+            'email_verified_at' => '2024-02-02 14:26:10',
+            'remember_token' => 'token',
+        ]);
     }
 }
