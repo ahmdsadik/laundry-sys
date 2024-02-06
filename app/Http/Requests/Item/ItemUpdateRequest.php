598ad93ec8afda0bf9dcdd964c46f20f8ccba71f
ziad-farg <ziad.farg.dev@gmail.com>
2024-02-06 14:38:42 +0200

    add authorize to request form

--- app/Http/Requests/Item/ItemUpdateRequest.php
+++ app/Http/Requests/Item/ItemUpdateRequest.php
@@ -2,13 +2,20 @@
 
 namespace App\Http\Requests\Item;
 
+use App\Enums\Roles;
 use Illuminate\Foundation\Http\FormRequest;
 
 class ItemUpdateRequest extends FormRequest
 {
     public function authorize(): bool
     {
-        return true;
+        // Check if the authenticated user is an admin
+        if (auth()->user()->role == Roles::ADMIN) {
+            return true;
+        }
+
+        // If not, abort with a 404 error
+        abort(404);
     }
 
     public function rules(): array
